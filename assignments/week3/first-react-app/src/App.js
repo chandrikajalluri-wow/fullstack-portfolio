d0754054025e49fb8f892e2dddd539268c0ee7ec
chandrikajalluri-wow <chandrika.jalluri@wowlabz.com>
2025-11-25 17:26:39 +0530

    chore: Successfully created my first react app with an aboutme page

---
+++ assignments/week3/first-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AboutMe from "./AboutMe";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My First Vite + React App</h1>
+      <AboutMe name="Chandrika" age={22} hobby="Coding and Music" />
+
+    </div>
+  );
+}
+
+export default App;
