a4d64152626f73238923eb2eb01de847f2a46484
chandrikajalluri-wow <chandrika.jalluri@wowlabz.com>
2025-11-26 15:50:15 +0530

    chore: created a demo page using reusable components.

---
+++ assignments/week3/first-react-app/src/components/ReusableComponents/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from "prop-types";
+const Button = ({ label, onClick, type, disabled }) => {
+  return (
+    <button onClick={onClick} type={type} disabled={disabled}>
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
+  disabled: PropTypes.bool,
+};
+
+export default Button;
